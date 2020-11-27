2eefee9cf718138724c364966606f97b7e7a45c2
David Hou <boy6284335@gmail.com>
2020-11-27 22:18:21 +0800

    add filtering function

--- read.py
+++ read.py
@@ -9,3 +9,12 @@ with open('reviews.txt', 'r') as f:
 		
 print("檔案讀取完了，總共有", len(data), "筆資料")
 print("每筆留言的平均長度為", len(words)/len(data))
+
+new = []
+count1 = 0
+for d in data:
+	if len(d) < 100:
+		count1 += 1
+	else:
+		pass
+print('總共有', count1, '筆資料長度小於100')
